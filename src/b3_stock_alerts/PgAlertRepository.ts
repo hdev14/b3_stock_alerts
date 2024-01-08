85db40a42473f5231cad81998f18b669ae4db6d1
Hermerson Araújo <ne.hermerson@gmail.com>
2024-01-08 09:53:02 -0300

    feat: finish PgAlertRepository implementation

--- src/b3_stock_alerts/PgAlertRepository.ts
+++ src/b3_stock_alerts/PgAlertRepository.ts
@@ -1,16 +1,36 @@
+import Postgres from "@shared/Postgres";
+import { Client } from "pg";
 import { Alert } from "./Alert";
 import AlertRepository from "./AlertRepository";
 
 export default class PgAlertRepository implements AlertRepository {
-  getAlert(alert_id: string): Promise<Alert | null> {
-    throw new Error("Method not implemented.");
+  private readonly client: Client;
+
+  constructor() {
+    this.client = Postgres.getClient();
   }
 
-  createAlert(alert: Alert): Promise<void> {
-    throw new Error("Method not implemented.");
+  async getAlert(alert_id: string): Promise<Alert | null> {
+    const result = await this.client.query(
+      'SELECT id, stock, user_id, max_amount, min_amount FROM alerts WHERE id = $1',
+      [alert_id]
+    );
+
+    if (result.rows[0] === undefined) {
+      return null;
+    }
+
+    return result.rows[0];
   }
-  
-  deleteAlert(alert_id: string): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async createAlert(alert: Alert): Promise<void> {
+    await this.client.query(
+      'INSERT INTO alerts (id, stock, user_id, max_amount, min_amount) VALUES ($1, $2, $3, $4, $5)',
+      [alert.id, alert.stock, alert.user_id, alert.max_amount, alert.min_amount]
+    );
+  }
+
+  async deleteAlert(alert_id: string): Promise<void> {
+    await this.client.query('DELETE FROM alerts WHERE id = $1', [alert_id]);
   }
 }
--- src/b3_stock_alerts/PgAlertRepository.unit.test.ts
+++ src/b3_stock_alerts/PgAlertRepository.unit.test.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { faker } from "@faker-js/faker/locale/pt_BR";
 import Postgres from "@shared/Postgres";
+import { Alert } from "./Alert";
 import PgAlertRepository from './PgAlertRepository';
 
 const getClientSpy = jest.spyOn(Postgres, 'getClient');
@@ -18,15 +20,69 @@ describe('PgAlertRepository', () => {
   });
 
   describe('PgAlertRepository.createAlert', () => {
-    it.todo('inserts a new alert')
+    it('inserts a new alert', async () => {
+      expect.assertions(1);
+
+      const alert: Alert = {
+        id: faker.string.uuid(),
+        stock: faker.string.alphanumeric(4),
+        user_id: faker.string.uuid(),
+        max_amount: faker.number.float(),
+        min_amount: faker.number.float(),
+      };
+
+      await repository.createAlert(alert);
+
+      expect(queryMock).toHaveBeenCalledWith(
+        'INSERT INTO alerts (id, stock, user_id, max_amount, min_amount) VALUES ($1, $2, $3, $4, $5)',
+        [alert.id, alert.stock, alert.user_id, alert.max_amount, alert.min_amount]
+      );
+    });
   });
 
   describe('PgAlertRepository.deleteAlert', () => {
-    it.todo('deletes an alert by id');
+    it('deletes an alert by id', async () => {
+      expect.assertions(1);
+
+      const alert_id = faker.string.uuid();
+
+      await repository.deleteAlert(alert_id);
+
+      expect(queryMock).toHaveBeenCalledWith('DELETE FROM alerts WHERE id = $1', [alert_id]);
+    });
   })
 
   describe('PgAlertRepository.getAlert', () => {
-    it.todo('returns an alert by id');
-    it.todo("returns NULL if alert doesn't exist");
+    it('returns an alert by id', async () => {
+      expect.assertions(2);
+
+      const alert: Alert = {
+        id: faker.string.uuid(),
+        stock: faker.string.alphanumeric(4),
+        user_id: faker.string.uuid(),
+        max_amount: faker.number.float(),
+        min_amount: faker.number.float(),
+      };
+
+      queryMock.mockResolvedValueOnce({ rows: [alert]});
+
+      const result = await repository.getAlert(alert.id);
+
+      expect(queryMock).toHaveBeenCalledWith('SELECT id, stock, user_id, max_amount, min_amount FROM alerts WHERE id = $1', [alert.id]);
+      expect(result).toEqual(alert);
+    });
+
+    it("returns NULL if alert doesn't exist", async () => {
+      expect.assertions(2);
+
+      queryMock.mockResolvedValueOnce({ rows: []  });
+
+      const alert_id = faker.string.uuid();
+
+      const result = await repository.getAlert(alert_id);
+
+      expect(queryMock).toHaveBeenCalledWith('SELECT id, stock, user_id, max_amount, min_amount FROM alerts WHERE id = $1', [alert_id]);
+      expect(result).toBeNull();
+    });
   });
 });
