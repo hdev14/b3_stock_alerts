6b30bcd6e4c8430d310d1fecf7678bc4676ade7c
Hermerson Araújo <ne.hermerson@gmail.com>
2024-01-14 14:00:53 -0300

    chore: setup DB and add SQL script

---
+++ script.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+	  id VARCHAR(32) NOT NULL,
+	  name VARCHAR(50) NOT NULL,
+ 	  email VARCHAR(50) NOT NULL,
+  	phone_number VARCHAR(20) NOT NULL,
+  	password text NOT NULL,
+  	CONSTRAINT users_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE alerts (
+	  id VARCHAR(32) NOT NULL,
+	  user_id VARCHAR(32) NOT NULL,
+  	stock VARCHAR(6) NOT NULL,
+  	max_amount float DEFAULT 0,
+  	min_amount float DEFAULT 0,
+  	CONSTRAINT alerts_pk PRIMARY KEY (id),
+  	FOREIGN KEY (user_id) REFERENCES users(id)
+);
