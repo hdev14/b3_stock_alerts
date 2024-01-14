e72e44ed5522c24a2a758eadc948277df8cb6c7f
Hermerson Araújo <ne.hermerson@gmail.com>
2024-01-14 15:29:35 -0300

    chore: create validator middleware

---
+++ src/api/middlewares/validator.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+
+export default function validator(request: Request, response: Response, next: NextFunction) {
+    const result = validationResult(request);
+    console.log(result);
+    
+    if (!result.isEmpty()) {
+      return response.status(400).json({ errors: result.array() })
+    }
+
+    return next();
+}
