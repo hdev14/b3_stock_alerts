69d82ecc56fe147b5684c251cb001b9ed4f75dfc
Hermerson Araújo <ne.hermerson@gmail.com>
2024-01-29 21:39:02 -0300

    chore: improve code

--- src/b3_stock_alerts/WSStockSearcher.ts
+++ src/b3_stock_alerts/WSStockSearcher.ts
@@ -3,6 +3,7 @@ import { load } from 'cheerio';
 import StockSearcher, { StockInfo } from "./SockSearcher";
 
 export default class WSStockSearcher implements StockSearcher {
+  private static CSS_selector = '.special > div:nth-child(1) > div:nth-child(1) > strong:nth-child(3)'
   async search(stock: string): Promise<StockInfo> {
     const response = await fetch(`https://statusinvest.com.br/acoes/${stock.toLocaleLowerCase()}`);
 
@@ -12,7 +13,7 @@ export default class WSStockSearcher implements StockSearcher {
 
     const buffer = Buffer.from(await response.arrayBuffer());
     const $ = load(buffer);
-    const stock_amount = $('.special > div:nth-child(1) > div:nth-child(1) > strong:nth-child(3)').text();
+    const stock_amount = $(WSStockSearcher.CSS_selector).text();
 
     return {
       amount: parseFloat(stock_amount.replace(',', '.')),
