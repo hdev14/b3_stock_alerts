1788f95a72d83d83d4e90fd789569bd13ce116dc
Hermerson Araújo <ne.hermerson@gmail.com>
2024-01-30 22:30:00 -0300

    feat: finish JobHandler.handle method

--- src/b3_stock_alerts/JobHandler.ts
+++ src/b3_stock_alerts/JobHandler.ts
@@ -1,12 +1,10 @@
 import AlertRepository from "@b3_stock_alerts/AlertRepository";
 import StockSearcher from "@b3_stock_alerts/SockSearcher";
+import StockNotFoundError from "@shared/StockNotFound";
 import { StockEvent } from "@shared/generic_types";
 import { EventEmitter } from "stream";
 import { Alert } from "./Alert";
 
-
-
-// TODO
 export default class JobHandler extends EventEmitter {
   constructor(
     private readonly alert_repository: AlertRepository,
@@ -17,39 +15,48 @@ export default class JobHandler extends EventEmitter {
 
   async handle() {
     let alerts: Alert[];
-    let skip = 1;
+    let skip = 0;
 
     do {
       alerts = await this.alert_repository.listAlerts({ limit: 10, skip });
 
-      await this.sendEvents(alerts);
+      await this.sendStockEvents(alerts);
 
-      skip++;
+      skip += 10;
     } while (alerts.length > 0);
   }
 
-  private async sendEvents(alerts: Alert[]) {
+  private async sendStockEvents(alerts: Alert[]) {
     for (let i = 0; i < alerts.length; i++) {
-      const alert = alerts[0];
-      const stock_info = await this.stock_searcher.search(alert.stock);
+      try {
+        const alert = alerts[i];
+        const stock_info = await this.stock_searcher.search(alert.stock);
 
-      if (alert.max_amount && alert.max_amount !== 0 && alert.max_amount <= stock_info.amount) {
-        this.emit('stock_event', {
-          current_amount: stock_info.amount,
-          isMax: true,
-          stock: alert.stock,
-          user_id: alert.user_id,
-        } as StockEvent);
-        continue;
-      }
+        if (alert.max_amount && alert.max_amount !== 0 && alert.max_amount <= stock_info.amount) {
+          this.emit('stock_event', {
+            current_amount: stock_info.amount,
+            isMax: true,
+            stock: alert.stock,
+            user_id: alert.user_id,
+          } as StockEvent);
+          continue;
+        }
 
-      if (alert.min_amount && alert.min_amount !== 0 && alert.min_amount >= stock_info.amount) {
-        this.emit('stock_event', {
-          current_amount: stock_info.amount,
-          isMax: false,
-          stock: alert.stock,
-          user_id: alert.user_id,
-        } as StockEvent);
+        if (alert.min_amount && alert.min_amount !== 0 && alert.min_amount >= stock_info.amount) {
+          this.emit('stock_event', {
+            current_amount: stock_info.amount,
+            isMax: false,
+            stock: alert.stock,
+            user_id: alert.user_id,
+          } as StockEvent);
+        }
+      } catch (e) {
+        if (e instanceof StockNotFoundError) {
+          console.log(e.message);
+          continue;
+        }
+
+        throw e;
       }
     }
   }
--- src/b3_stock_alerts/JobHandler.unit.test.ts
+++ src/b3_stock_alerts/JobHandler.unit.test.ts
@@ -1,4 +1,5 @@
 import { faker } from "@faker-js/faker/locale/pt_BR";
+import StockNotFoundError from "@shared/StockNotFound";
 import JobHandler from "./JobHandler";
 
 describe('JobHandler unit tests', () => {
@@ -16,16 +17,44 @@ describe('JobHandler unit tests', () => {
 
   const handler = new JobHandler(alert_repository_mock, stock_searcher_mock);
 
-
   beforeEach(() => {
     alert_repository_mock.listAlerts.mockClear();
     stock_searcher_mock.search.mockClear();
   });
 
-  it.todo("doesn't stop if some stock doesn't exist")
+  it("doesn't stop if some stock doesn't exist", async () => {
+    expect.assertions(1);
+
+    const alerts = [
+      {
+        id: faker.string.uuid(),
+        stock: faker.string.alphanumeric(4),
+        user_id: faker.string.uuid(),
+        max_amount: 100,
+        min_amount: 0,
+      },
+      {
+        id: faker.string.uuid(),
+        stock: faker.string.alphanumeric(4),
+        user_id: faker.string.uuid(),
+        max_amount: 0,
+        min_amount: 10,
+      }
+    ];
+
+    alert_repository_mock.listAlerts
+      .mockResolvedValueOnce(alerts)
+      .mockResolvedValueOnce([]);
+
+    stock_searcher_mock.search
+      .mockRejectedValueOnce(new StockNotFoundError('test'))
+      .mockResolvedValueOnce({ amount: 101 });
+
+    await expect(handler.handle()).resolves.not.toThrow();
+  })
 
   it('sends a stock event for each confirmed stock amount', async () => {
-    expect.assertions(9);
+    expect.assertions(11);
 
     const alerts = [
       {
@@ -59,13 +88,14 @@ describe('JobHandler unit tests', () => {
 
 
     expect(emit_spy).toHaveBeenCalledTimes(2);
-    // expect(emit_spy.mock.calls[0][0]).toEqual('stock_event');
+
+    expect(emit_spy.mock.calls[0][0]).toEqual('stock_event');
     expect(emit_spy.mock.calls[0][1].current_amount).toEqual(101);
     expect(emit_spy.mock.calls[0][1].user_id).toEqual(alerts[0].user_id);
     expect(typeof emit_spy.mock.calls[0][1].isMax).toEqual('boolean');
     expect(emit_spy.mock.calls[0][1].stock).toEqual(alerts[0].stock);
 
-    // expect(emit_spy.mock.calls[1][0]).toEqual('stock_event');
+    expect(emit_spy.mock.calls[1][0]).toEqual('stock_event');
     expect(emit_spy.mock.calls[1][1].current_amount).toEqual(9);
     expect(emit_spy.mock.calls[1][1].user_id).toEqual(alerts[1].user_id);
     expect(typeof emit_spy.mock.calls[1][1].isMax).toEqual('boolean');
