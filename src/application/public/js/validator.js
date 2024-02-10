cf0af21f6ca34f2a3116b8a21270f6f89081aa9a
Hermerson Araújo <ne.hermerson@gmail.com>
2024-02-10 14:39:04 -0300

    refactor: improve validator

--- src/application/public/js/validator.js
+++ src/application/public/js/validator.js
@@ -1,104 +1,105 @@
-const URL_REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w\-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)/;
-const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*/;
-
-const RULE_FUNCTIONS = {
-  required: (value) => {
-    switch (typeof value) {
-      case 'number':
-        return (value === undefined || value === null);
-      case 'string':
-        return (value === undefined || value === null || value.length === 0);
-      case 'object':
-        // don't use it with Date.
-        return (
-          value === undefined
-          || value === null
-          || Object.keys(value).length === 0
-        );
-      default:
-        return true;
-    }
-  },
-  max: (value, max) => {
-    if (value !== undefined) {
-      switch (typeof value) {
-        case 'number':
-          return value > max;
-        case 'string':
-          return value.length > max;
-        default:
-          return true;
-      }
-    }
-    return false;
-  },
-  min: (value, min) => {
-    if (value !== undefined) {
-      switch (typeof value) {
-        case 'number':
-          return value < min;
-        case 'string':
-          return value.length < min;
-        default:
-          return true;
-      }
-    }
-
-    return false;
-  },
-  url: (value) => (value !== undefined && typeof value === 'string' && !URL_REGEX.test(value)),
-  string: (value) => (value !== undefined && typeof value !== 'string'),
-  number: (value) => (value !== undefined && typeof value !== 'number'),
-  integer: (value) => (value !== undefined && typeof value === 'number' && !Number.isInteger(value)),
-  float: (value) => (value !== undefined && typeof value === 'number' && Number.isInteger(value)),
-  date: (value) => (value !== undefined && !(value instanceof Date)),
-  email: (value) => (value !== undefined && typeof value === 'string' && !EMAIL_REGEX.test(value)),
-  password: (value) => (
-    value !== undefined
-    && typeof value === 'string'
-    && !(
-      value.includes('!')
-      || value.includes('@')
-      || value.includes('#')
-      || value.includes('$')
-      || value.includes('%')
-      || value.includes('&')
-      || value.includes('*')
-      || value.includes('(')
-      || value.includes(')')
-      || value.includes('_')
-      || value.includes('+')
-    )
-  ),
-};
-
-const RULE_MESSAGES = {
-  required: () => 'O campo é obrigatório.',
-  max: (max, isString) => {
-    if (isString) {
-      return `O texto precisa ter até ${max} caracteres.`;
-    }
-    return `O campo menor ou igual à ${max}.`;
-  },
-  min: (min, isString) => {
-    if (isString) {
-      return `O texto precisa ter pelo menos ${min} caracteres.`;
-    }
-
-    return `O campo preicsa maior ou igual à ${min}.`;
-  },
-  url: () => 'O campo precisa ser uma URL.',
-  string: () => 'O campo precisa ser um texto.',
-  number: () => 'O campo precisa ser um número.',
-  integer: () => 'O campo precisa ser um inteiro.',
-  float: () => 'O campo precisa ser um decimal.',
-  date: () => 'O campo precisa ser uma data válida.',
-  email: () => 'O campo precisa ser um e-mail válido.',
-  password: () => 'O campo precisa ter letras, números e algum caracter especial.',
-};
-
 // eslint-disable-next-line no-unused-vars
 class Validator {
+  #URL_REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w\-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)/;
+
+  #EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*/;
+
+  #RULE_FUNCTIONS = {
+    required: (value) => {
+      switch (typeof value) {
+        case 'number':
+          return (value === undefined || value === null);
+        case 'string':
+          return (value === undefined || value === null || value.length === 0);
+        case 'object':
+          // don't use it with Date.
+          return (
+            value === undefined
+            || value === null
+            || Object.keys(value).length === 0
+          );
+        default:
+          return true;
+      }
+    },
+    max: (value, max) => {
+      if (value !== undefined) {
+        switch (typeof value) {
+          case 'number':
+            return value > max;
+          case 'string':
+            return value.length > max;
+          default:
+            return true;
+        }
+      }
+      return false;
+    },
+    min: (value, min) => {
+      if (value !== undefined) {
+        switch (typeof value) {
+          case 'number':
+            return value < min;
+          case 'string':
+            return value.length < min;
+          default:
+            return true;
+        }
+      }
+
+      return false;
+    },
+    url: (value) => (value !== undefined && typeof value === 'string' && !this.#URL_REGEX.test(value)),
+    string: (value) => (value !== undefined && typeof value !== 'string'),
+    number: (value) => (value !== undefined && typeof value !== 'number'),
+    integer: (value) => (value !== undefined && typeof value === 'number' && !Number.isInteger(value)),
+    float: (value) => (value !== undefined && typeof value === 'number' && Number.isInteger(value)),
+    date: (value) => (value !== undefined && !(value instanceof Date)),
+    email: (value) => (value !== undefined && typeof value === 'string' && !this.#EMAIL_REGEX.test(value)),
+    password: (value) => (
+      value !== undefined
+      && typeof value === 'string'
+      && !(
+        value.includes('!')
+        || value.includes('@')
+        || value.includes('#')
+        || value.includes('$')
+        || value.includes('%')
+        || value.includes('&')
+        || value.includes('*')
+        || value.includes('(')
+        || value.includes(')')
+        || value.includes('_')
+        || value.includes('+')
+      )
+    ),
+  };
+
+  #RULE_MESSAGES = {
+    required: () => 'O campo é obrigatório.',
+    max: (max, isString) => {
+      if (isString) {
+        return `O texto precisa ter até ${max} caracteres.`;
+      }
+      return `O campo menor ou igual à ${max}.`;
+    },
+    min: (min, isString) => {
+      if (isString) {
+        return `O texto precisa ter pelo menos ${min} caracteres.`;
+      }
+
+      return `O campo preicsa maior ou igual à ${min}.`;
+    },
+    url: () => 'O campo precisa ser uma URL.',
+    string: () => 'O campo precisa ser um texto.',
+    number: () => 'O campo precisa ser um número.',
+    integer: () => 'O campo precisa ser um inteiro.',
+    float: () => 'O campo precisa ser um decimal.',
+    date: () => 'O campo precisa ser uma data válida.',
+    email: () => 'O campo precisa ser um e-mail válido.',
+    password: () => 'O campo precisa ter letras, números e algum caracter especial.',
+  };
+
   #data;
 
   #rule_map = new Map();
@@ -168,10 +169,10 @@ class Validator {
     for (let i = 0, len = rule.length; i < len; i += 1) {
       const ruleString = rule[i];
       const [name, value] = ruleString.split(':');
-      const isNotValid = RULE_FUNCTIONS[name](field, value);
+      const isNotValid = this.#RULE_FUNCTIONS[name](field, value);
 
       if (isNotValid) {
-        const message = RULE_MESSAGES[name](value, typeof field === 'string');
+        const message = this.#RULE_MESSAGES[name](value, typeof field === 'string');
         messages.push(message);
       }
     }
