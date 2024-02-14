3390db3abff935a40602ab3bbc417c0f995031ea
Hermerson Araújo <ne.hermerson@gmail.com>
2024-02-14 18:39:54 -0300

    chore: improve minify script to also copy other files

--- minify.sh
+++ minify.sh
@@ -1,8 +1,11 @@
 echo "Copying pages files...\n"
 
-cp -rf src/application/pages/ build/src/application/pages/
+cp -rf ./src/application/pages/ build/./src/application/pages/
 find ./build/src/application/pages -name "*.ts" | xargs rm -r
-cp -rf src/application/public/css/ build/src/application/public/css/
+cp -rf ./src/application/public/css/ ./build/src/application/public/css/
+find ./src/application/public -name "*.png" | xargs -i cp {} ./build/src/application/public/
+cp ./src/application/public/favicon.ico ./build/src/application/public/
+cp ./src/application/public/site.webmanifest ./build/src/application/public/
 
 echo "Minifying JS...\n"
 npx esbuild build/src/application/public/js/*.js --minify --out-extension:.js=.min.js --outdir=build/src/application/public/js/
