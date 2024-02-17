3661cb99c40b7ca415d3e1a0b76b14a2480c073f
Hermerson Araújo <ne.hermerson@gmail.com>
2024-02-16 22:55:13 -0300

    refactor: improve code

--- src/application/middlewares/auth.ts
+++ src/application/middlewares/auth.ts
@@ -5,8 +5,6 @@ import {
 import { auth_service } from 'src/bootstrap';
 
 export default function auth(request: Request, response: Response, next: NextFunction) {
-  const is_page = isPage(request.originalUrl);
-
   if (request.headers.cookie) {
     const cookie = request.headers.cookie.split(';').find((cookie) => cookie.split('=')[0] === 'AT');
     const [, access_token] = cookie !== undefined ? cookie.split('=') : [];
@@ -19,7 +17,7 @@ export default function auth(request: Request, response: Response, next: NextFun
     }
   }
 
-  return is_page ? response.redirect('/pages/login') : response.status(401).json({
-    message: 'É necessário está autenticado.',
-  });
+  return isPage(request.originalUrl)
+    ? response.redirect('/pages/login')
+    : response.status(401).json({ message: 'É necessário está autenticado.' });
 }
